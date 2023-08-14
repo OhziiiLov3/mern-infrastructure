c5db5f7cb85a57bfdfac28ca91b103b43ea58f58
ohziiilov3 <klbaskerville0520@gmail.com>
2023-08-14 07:14:38 -0700

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './pages/App/App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+    
+      <App />
+   
+  </React.StrictMode>
+);
